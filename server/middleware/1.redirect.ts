d805fb517adbac671fb8feac7a5460cb6a947a98
JiJi <i@mmdjiji.com>
2024-08-30 04:34:57 +0000

    slug

--- server/middleware/1.redirect.ts
+++ server/middleware/1.redirect.ts
@@ -7,7 +7,7 @@ export default eventHandler(async (event) => {
   const { slugRegex, reserveSlug } = useAppConfig(event)
   const { homeURL, linkCacheTtl, redirectWithQuery } = useRuntimeConfig(event)
   const { cloudflare } = event.context
-  return sendRedirect(event, event.path.slice(1))
+  return sendRedirect(event, slug)
   if (event.path === '/' && homeURL)
     return sendRedirect(event, homeURL)
 
